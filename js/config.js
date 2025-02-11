ba5e79c42760b55bb2bdeade5abffdc68544c272
LukaVucinic <159730295+LukaVucinic@users.noreply.github.com>
2025-02-12 00:48:41 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_wkhEzX2JA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lnhd6qjras5cedg79m8h4kgq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_532TWZ2gZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5o6b1112f8igor6ldl3ig1b2hj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
