1ea0539a734c0796ac5a0b98260668eb5ece1b42
LukaVucinic <159730295+LukaVucinic@users.noreply.github.com>
2025-02-12 00:38:22 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_wkhEzX2JA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'lnhd6qjras5cedg79m8h4kgq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
