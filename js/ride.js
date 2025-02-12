a26c2979e203bab54f3c9c02cafee870c7b7fa31
LukaVucinic <159730295+LukaVucinic@users.noreply.github.com>
2025-02-12 01:51:22 +0100

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -72,7 +72,7 @@ WildRydes.map = WildRydes.map || {};
 
     function handlePickupChanged() {
         var requestButton = $('#request');
-        requestButton.text('Request Unicorn');
+        requestButton.text('Request Uber');
         requestButton.prop('disabled', false);
     }
 
